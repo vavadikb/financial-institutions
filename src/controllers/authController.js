834096958225954a4af85156e6dc0c3cdb095fba
Вадим Бабаев <0463503@gmail.com>
2023-03-20 23:01:57 +0100

    realised jwt auth without libraries

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,62 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import CryptoJS from "crypto-js";
 import { pool } from "../dbConnection.js";
 
+const secret = process.env.SECRET;
+
 const generateAccessToken = (username) => {
-  const payload = {
-    username,
-  };
-  return jwt.sign(payload, process.env.SECRET, { expiresIn: "24h" });
+  const header = { alg: "HS256", typ: "JWT" };
+  const payload = { username };
+  const encodedHeader = CryptoJS.enc.Base64.stringify(
+    CryptoJS.enc.Utf8.parse(JSON.stringify(header))
+  );
+  const encodedPayload = CryptoJS.enc.Base64.stringify(
+    CryptoJS.enc.Utf8.parse(JSON.stringify(payload))
+  );
+  const signature = CryptoJS.HmacSHA256(
+    encodedHeader + "." + encodedPayload,
+    secret
+  );
+  const encodedSignature = CryptoJS.enc.Base64.stringify(signature);
+  const token = `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
+  return token;
+};
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  const [encodedHeader, encodedPayload, encodedSignature] = token.split(".");
+  const header = JSON.parse(
+    CryptoJS.enc.Base64.parse(encodedHeader).toString(CryptoJS.enc.Utf8)
+  );
+  const payload = JSON.parse(
+    CryptoJS.enc.Base64.parse(encodedPayload).toString(CryptoJS.enc.Utf8)
+  );
+  const secret = process.env.SECRET;
+  const signature = CryptoJS.enc.Base64.parse(encodedSignature);
+
+  try {
+    const isVerified = CryptoJS.HmacSHA256(
+      encodedHeader + "." + encodedPayload,
+      secret
+    ).toString();
+
+    if (
+      isVerified !== CryptoJS.enc.Hex.parse(signature.toString()).toString()
+    ) {
+      return res.sendStatus(403);
+    }
+
+    req.user = payload;
+    next();
+  } catch (err) {
+    console.error("Error authentication", err);
+    return res.sendStatus(500);
+  }
 };
 
 export async function registration(req, res) {
@@ -20,22 +70,20 @@ export async function registration(req, res) {
       if (result.rows.length > 0) {
         return res
           .status(400)
-          .json({ message: `user with username ${username} already created` });
+          .json({ message: `User with username ${username} already created` });
       }
 
       // Hash the password and create a new user record
-      const hashPassword = bcrypt.hashSync(password, 7);
+      const hashPassword = CryptoJS.SHA256(password).toString();
       const insertText = "INSERT INTO users(username, password) VALUES($1, $2)";
       await pool.query(insertText, [username, hashPassword]);
 
       return res.json({
-        message: `User ${username}succsuccessfully registered`,
+        message: `User ${username} successfully registered`,
       });
     } catch (err) {
       console.error("Error during registration", err);
       return res.status(500).json({ message: "Internal server error" });
-    } finally {
-      pool.release();
     }
   } catch (err) {
     console.error("Error connecting to database", err);
@@ -52,14 +100,14 @@ export async function login(req, res) {
     );
 
     if (result.rowCount === 0) {
-      return res.status(400).json({ message: `user ${username} not found` });
+      return res.status(400).json({ message: `User ${username} not found` });
     }
 
     const user = result.rows[0];
+    const hashPassword = CryptoJS.SHA256(password).toString();
 
-    const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) {
-      return res.status(400).json({ message: `bad password` });
+    if (user.password !== hashPassword) {
+      return res.status(400).json({ message: "Bad password" });
     }
 
     const token = generateAccessToken(username);
